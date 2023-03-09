2f2fff0070d0e236a808bd1ad67b4b6d7732ab51
jafetejr <jafetejr@gmail.com>
2023-03-09 20:17:44 -0300

    Aula Operadores 1

---
+++ java-anatomia-classes/src/edu/jafete/java/Operadores/Operadores.java
@@ -0,0 +1,18 @@
+package edu.jafete.java.Operadores;
+
+public class Operadores {
+    
+    public static void main (String[] args) {
+
+        String nomeCompleto = "jafete " + "ribeiro";
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+        System.out.println(concatenacao);
+        
+        concatenacao = 2 + 2 + "1" + 2 + 2;
+        System.out.println(concatenacao);
+
+    }
+    
+}
