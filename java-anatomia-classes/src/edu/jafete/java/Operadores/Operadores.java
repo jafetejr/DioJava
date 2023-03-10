34945b679d03fbf2262224fb3d659bb6d299ff34
jafetejr <jafetejr@gmail.com>
2023-03-09 21:08:16 -0300

    Estudos 09032023

--- java-anatomia-classes/src/edu/jafete/java/Operadores/Operadores.java
+++ java-anatomia-classes/src/edu/jafete/java/Operadores/Operadores.java
@@ -13,6 +13,47 @@ public class Operadores {
         concatenacao = 2 + 2 + "1" + 2 + 2;
         System.out.println(concatenacao);
 
+        String nomeAula = "AULA 2: INCREMENTO";
+        System.out.println(nomeAula);
+
+        int numero = 1;
+        numero = ++ numero;
+        System.out.println(numero ++);
+        
+        System.out.println(numero);
+
+        nomeAula = "AULA 5: CONDICIONAL";
+        System.out.println(nomeAula);
+
+        int a,b;
+        a = 51;
+        b = 7;
+
+        String resultado = a==b ?"verdadeiro" : "falso";
+        System.out.println(resultado);
+
+        nomeAula = "AULA 5: RELACIONAL";
+        System.out.println(nomeAula);
+
+        Boolean simNao = a == b;
+        System.out.println(simNao);
+
+        simNao = a != b;
+        System.out.println(simNao);
+
+        simNao = a >= b;
+        System.out.println(simNao);
+
+        if(a < b){
+            System.out.println("Condição aceita");
+        } else {System.out.println("Condição negada");}
+            
+        
+
+
+
     }
+
+    
     
 }
