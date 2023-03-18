7ba1bda9734920df05401b13267d6a81fb18bfdb
jafetejr <99699887+jafetejr@users.noreply.github.com>
2023-03-18 15:07:15 -0300

    Exercicio Consoantes

---
+++ controle-fluxos/src/Consoantes/Consoantes.java
@@ -0,0 +1,41 @@
+package Consoantes;
+
+import java.util.Scanner;
+
+public class Consoantes {
+
+    
+    
+    public static void main(String[] args) throws Exception{
+        Scanner scanner = new Scanner(System.in);
+        String[] letra = new String[6];
+        String[] consoantes = new String[6];
+        int contadorConsoantes = 0;
+
+        for (int x=0; x<5; x++){
+            System.out.print("Digite uma letra: ");
+            letra[x] = scanner.next();
+        }
+
+        for (int x=0; x<5; x++){
+            if (!(letra[x].equalsIgnoreCase("a")) |
+                (letra[x].equalsIgnoreCase( "e")) |
+                (letra[x].equalsIgnoreCase( "i")) |
+                (letra[x].equalsIgnoreCase( "o")) |
+                (letra[x].equalsIgnoreCase( "u"))){
+                
+                
+                consoantes[contadorConsoantes] = letra[x];
+                contadorConsoantes++;
+            }
+        }
+
+        System.out.println("Foram digitadas " + contadorConsoantes + " consoantes.");
+        System.out.print("As consoantes digitadas foram: ");
+        for (int x=0; x<contadorConsoantes; x++){
+            System.out.print(consoantes[x]);
+
+        }
+
+    }
+}
--- controle-fluxos/src/Meses/Meses.java
+++ controle-fluxos/src/Meses/Meses.java
@@ -1,3 +1,4 @@
+package Meses;
 import java.util.Scanner;
 
 public class Meses {
